b7604bfd796b52e4d9f653771822140d9b615684
BraveSejin <ihavebravebiz@gmail.com>
2022-07-31 20:02:02 +0900

    뷰모델로 관심사 분리 -> 토스트로 1초후 사이즈 출력

--- app/src/main/java/com/sejin/example/sunflower_clone/GalleryFragment.kt
+++ app/src/main/java/com/sejin/example/sunflower_clone/GalleryFragment.kt
@@ -1,23 +1,22 @@
 package com.sejin.example.sunflower_clone
 
+import GalleryViewModel
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
-import com.sejin.example.sunflower_clone.adapters.UnsplashApi
-import com.sejin.example.sunflower_clone.adapters.UnsplashService
-import com.sejin.example.sunflower_clone.data.UnsplashSearchResponse
 import com.sejin.example.sunflower_clone.databinding.FragmentGalleryBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class GalleryFragment : Fragment() {
 
     val args: PlantDetailFragmentArgs by navArgs()
+
+    private lateinit var viewModel: GalleryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,24 +30,15 @@ class GalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val call = UnsplashApi.retrofitService.searchPhotos(
-            "apple",
-            10,
-            10
-        )
-        val callback = object : Callback<UnsplashSearchResponse> {
-            override fun onResponse(
-                call: Call<UnsplashSearchResponse>,
-                response: Response<UnsplashSearchResponse>
-            ) {
-                Log.i("TTAAGG", "onResponse: ${response.body()}")
-            }
+        viewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
+        showToastDelayed()
 
-            override fun onFailure(call: Call<UnsplashSearchResponse>, t: Throwable) {
-                Log.i("TTAAGG", "onFailure: ${t.message}")
-            }
-        }
+    }
 
-        call.enqueue(callback)
+    private fun showToastDelayed() {
+        Handler().postDelayed({
+            Toast.makeText(requireContext(), "${viewModel.photos.size}", Toast.LENGTH_SHORT).show()
+        }, 3000)
+
     }
 }
